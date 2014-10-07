2d7e8980d0ad0c76620dc09a54e87094f5a614d1
Wendy Dherin <wendydherin@gmail.com>
2014-10-06 22:13:22 -0700

    Generated a list of tuple pairs, where first item in tuple is the letter of the alphabet and the second item is 0.

--- lettercount.py
+++ lettercount.py
@@ -7,7 +7,11 @@ def main():
 	fin = open(filename)
 	content = fin.read().lower()
 
-	print content
+	letter_list = []
+	alphabet = string.lowercase
+	for letter in alphabet:
+		letter_list.append((letter, 0))
+	print letter_list
 
 if __name__ == "__main__":
 	main()
