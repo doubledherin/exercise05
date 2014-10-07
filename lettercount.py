476bc8b9d6a9b0a755375c22fed866d6e128a5a2
Wendy Dherin <wendydherin@gmail.com>
2014-10-06 22:20:06 -0700

    Changed tack to two aligned lists.

--- lettercount.py
+++ lettercount.py
@@ -4,14 +4,25 @@ import string
 script, filename = argv
 
 def main():
-	fin = open(filename)
-	content = fin.read().lower()
 
-	letter_list = []
+	# Initialize a list letters in the alphabet
+	letters = []
 	alphabet = string.lowercase
 	for letter in alphabet:
-		letter_list.append((letter, 0))
-	print letter_list
+		letters.append(letter)
+	print letters
+
+	# Initialize a list of 26 zeros to align with letter_list
+	counts = [0] * 26
+	print counts
+
+
+	fin = open(filename)
+	content = fin.read().lower()
+	for char in content:
+		pass
+
+
 
 if __name__ == "__main__":
 	main()
